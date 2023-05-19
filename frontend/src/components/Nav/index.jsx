313702e896c2bf8b68c578fc90900d35fe557563
jimcreel <jim.creel@gmail.com>
2023-05-19 13:18:46 -0700

    styling updates and abstraction

--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -1,21 +1,30 @@
-import { Link } from 'react-router-dom'
-export default function Nav () {
-    return (
-        <>
-        
-        <nav className='bg-black text-secondary font-sans text-xl' >
-            <ul className='flex flex-row justify-start items-center' >
-            <Link to="/" ><li className='m-2'>Home</li></Link>
-            <Link to="/about"><li className='m-2'>About</li></Link>
-            <Link to="/projects"><li className='m-2'>Projects</li></Link>
-            <li className='m-2'><a href='https://github.com/jimcreel'><img src='/assets/img/github-mark-white.svg' className='w-[25px]'/></a></li>
-            <li className='m-2'><a href='https://www.linkedin.com/in/jim-creel'><img src='/assets/img/icons8-linkedin.svg' className='w-[25px]'/></a></li>
-            <li className='m-2'><a href='mailto: jim.creel@gmail.com'><img src='/assets/img/Aiga_mail_white.svg.png' className='w-[25px]'/></a></li>
-            
-            </ul>
-            
-        </nav>
-        
-        </>
-    )
+import { Link } from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <nav className='bg-black text-secondary font-sans text-xl'>
+      <ul className='flex flex-row justify-start items-center'>
+        <li className='m-2'>
+          <Link to="/">Home</Link>
+        </li>
+        <li className='m-2'>
+          <Link to="/about">About</Link>
+        </li>
+        <li className='m-2'>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li className='ml-auto flex justify-end space-x-2'>
+          <a href='https://github.com/jimcreel' target='_blank' rel='noopener noreferrer'>
+            <img src='/assets/img/github-mark-white.svg' className='w-[25px]' alt='GitHub' />
+          </a>
+          <a href='https://www.linkedin.com/in/jim-creel' target='_blank' rel='noopener noreferrer'>
+            <img src='/assets/img/icons8-linkedin.svg' className='w-[25px]' alt='LinkedIn' />
+          </a>
+          <a href='mailto:jim.creel@gmail.com'>
+            <img src='/assets/img/Aiga_mail_white.svg.png' className='w-[25px]' alt='Email' />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- frontend/src/components/ProjectCard/index.jsx
+++ frontend/src/components/ProjectCard/index.jsx
@@ -15,9 +15,9 @@ export default function ProjectCard(props) {
       <ReactCardFlip
         isFlipped={isFlipped}
         flipDirection="horizontal"
-        className="w-[200px] "
+        className="w-[250px] "
       >
-        <div className="border-2 border-secondary w-[200px] text-accent shadow-sm rounded p-5 m-2 text-center ">
+        <div className="border-2 border-secondary w-[250px] text-accent shadow-sm rounded p-5 m-2 text-center ">
           <button onClick={handleClick}>
             <div className="h-[150px]">
               <img src={cardData.img} alt={cardData.title} className='max-w-[75%], max-h-[75%]' />
@@ -25,13 +25,14 @@ export default function ProjectCard(props) {
             <p className="text-accent text-2xl">{cardData.title}</p>
           </button>
         </div>
-        <div className="border-2 border-secondary w-[200px] text-accent shadow-sm rounded p-5 m-2 flex flex-col align-center">
+        <div className="border-2 border-secondary w-[250px] text-accent shadow-sm rounded p-5 m-2 flex flex-col align-center">
           <button onClick={handleClick}>
             <p>{cardData.text}</p>
           </button>
+          {cardData.link &&
           <button className="bg-secondary text-accent rounded p-2 m-2">
             <a href={cardData.link} target="_blank" rel="noopener noreferrer">Visit Site</a>
-          </button>
+          </button>}
         </div>
       </ReactCardFlip>
     </>
