0cf22e41c17f9df209af58b1bf8504b013342ed8
jimcreel <jim.creel@gmail.com>
2023-05-19 14:52:23 -0700

    updated https to http

--- frontend/src/components/ProjectsPage/index.jsx
+++ frontend/src/components/ProjectsPage/index.jsx
@@ -12,7 +12,7 @@ export default function ProjectsPage() {
             The site pulls publicly available reservation data from Disney websites and displays it for the
               user filtered by resort, park, date, and annual pass. Requires a Google account to use.`,
             img: '/assets/img/Disneyland.png',
-            link: 'https://magicreservations.jim-creel.com/'
+            link: 'http://magicreservations.jim-creel.com/'
             
         }, 
         {
@@ -20,14 +20,14 @@ export default function ProjectsPage() {
             text: `Brick By Brick is a Django/Python LEGO inventory site powered by a postgreSQL database, 
             where you can track your LEGO sets and parts.`,
             img: '/assets/img/BrickByBrick.png',
-            link: 'https://brickbybrick.jim-creel.com/'
+            link: 'http://brickbybrick.jim-creel.com/'
         },
         {
             
             text: `MLB Info is a dynamic single page ReactJS site styled with Tailwind and powered by an 
             ExpressJS, NodeJS, and MongoDB backend.`,
             img: '/assets/img/MLBInfo.png',
-            link: 'https://mlbscores.jim-creel.com/'
+            link: 'http://mlbscores.jim-creel.com/'
         }
 
     ])
