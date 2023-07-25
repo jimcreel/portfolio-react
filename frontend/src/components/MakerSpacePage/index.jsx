57613b6a3cf189fd1c33d9d2b15343ea120e7dee
Jim Creel <jim.creel@gmail.com>
2023-07-24 20:52:56 -0700

    adjusted styling for makerspace page

--- frontend/src/components/MakerSpacePage/index.jsx
+++ frontend/src/components/MakerSpacePage/index.jsx
@@ -1,22 +1,11 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-
 export default function MakerSpacePage() {
     return(
         <>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center align-center">
                 <h1 className="font-bold text-5xl text-accent text-center p-10">
                     Custom Raspberry Pi Arcade Project </h1>
-                <Carousel
-                    autoPlay
-                    showThumbs={false}
-                    showStatus={false}
-                    centerMode={true}
-                    centerSlidePercentage={30}
-                    infiniteLoop={true}
-                >
-                
-                            <div className='max-w-sm'>
+                        <div className='flex flex-col justify-between w-[70%] text-secondary items-center'>
+                            <div className='w-[70%]'>
                                 <h2 className="font-bold text-3xl text-accent text-center p-10">
                                     Prototype: a carboard template for proof of concept
                                 </h2>
@@ -24,7 +13,7 @@ export default function MakerSpacePage() {
                                     className="object-contain rounded border-2 border-accent"
                                     src='assets/img/Prototype.PNG'></img>
                             </div>
-                            <div className='max-w-sm'>
+                            <div className='w-[70%]'>
                                 <h2 className = 'font-bold text-3xl text-accent text-center p-10'>
                                     PrototypeV2: mdf prototype with an acrylic top
                                 </h2>
@@ -32,7 +21,7 @@ export default function MakerSpacePage() {
                                     className = 'object-contain rounded border-2 border-accent'
                                     src='assets/img/PrototypeV2.JPG'></img>
                             </div>
-                            <div className='max-w-sm'>
+                            <div className='w-[70%]'>
                                 <h2 className = 'font-bold text-3xl text-accent text-center p-10'>
                                     PrototypeV2: buttons and joysticks installed and tested
                                 </h2>
@@ -40,7 +29,7 @@ export default function MakerSpacePage() {
                                     className = 'object-contain rounded border-2 border-accent'
                                     src='assets/img/PrototypeV3.JPG'></img>
                             </div>
-                            <div className='max-w-sm'>
+                            <div className='w-[70%]'>
                                 <h2 className = 'font-bold text-3xl text-accent text-center p-10'>
                                     Final: MDF top with acrylic overlay, plywood sides with cutouts for moving
                                 </h2>
@@ -48,7 +37,7 @@ export default function MakerSpacePage() {
                                     className = 'object-contain rounded border-2 border-accent'
                                     src='assets/img/Final.JPEG'></img>
                             </div>
-                            <div className='max-w-sm'>
+                            <div className='w-[70%]'>
                                 <h2 className = 'font-bold text-3xl text-accent text-center p-10'>
                                     Final:  Piano hinge top for easy access to the inside
                                 </h2>
@@ -56,8 +45,8 @@ export default function MakerSpacePage() {
                                     className = 'object-contain rounded border-2 border-accent'  
                                     src='assets/img/FinalSide.JPEG'></img>
                             </div>
-                    
-                </Carousel>
+            </div>                    
+                
             </div>
         </>
     )
